1da02484e1a8b3fc65850b861635df145a520f16
ethem <88254285+ethemtion@users.noreply.github.com>
2022-06-24 15:13:47 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+from gettext import gettext
+from tkinter import *
+import requests
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+# configure workspace
+ws = Tk()
+ws.title("Blacklist Sorgulama")
+ws.geometry("500x250")
+ws.configure(bg='#1b5563')
+
+driver = webdriver.Firefox()
+driver.get("https://mxtoolbox.com/blacklists.aspx" )
+driver.minimize_window()
+
+
+def btnClick():
+    Label(ws, text="", pady=15, padx=10, bg="#1b5563")
+    urlsInput= urlTb.get()
+    #page = requests.get(url)
+    
+    urls = urlsInput.split(' ')
+    for url in urls:
+        driver.get("https://mxtoolbox.com/blacklists.aspx" )
+
+        with open('output.txt', 'w') as f:
+            f.write("")
+        
+        checkTxt = driver.find_element(by=By.NAME, value="ctl00$ContentPlaceHolder1$ucToolhandler$txtToolInput")
+        checkTxt.clear()
+        checkTxt.send_keys(url)
+        checkBtn = driver.find_element(by=By.NAME, value="ctl00$ContentPlaceHolder1$ucToolhandler$btnAction")
+        checkBtn.click()
+        time.sleep(5)
+        # result = driver.find_element(by=By.CLASS_NAME, value="tool-result-body")
+        # print(result.get_attribute('value'))
+        
+        result = driver.find_element(by=By.XPATH, value='/html/body/form/div[3]/div[2]/div[2]/div/div[2]/div[2]/span/div/div[3]')
+        result_text = str(result.text)
+        with open('output.txt', 'a') as f:
+                f.write(result_text)
+                f.write('\n'*10)
+        
+        Label(ws, text="İşlem tamamlandı", pady=15, bg='#1b5563').grid(row=2 , rowspan=1)
+    
+    
+
+    # soup = BeautifulSoup(page.text,"html5lib")
+    # print(soup.prettify())
+    return 0
+    
+
+urlLbl = Label(ws, text="URL'i girin", pady=15, padx=10, bg='#1b5563')
+urlTb = Entry(ws)
+
+findBtn = Button(ws, text=" Bul! ", command=btnClick )
+
+urlLbl.grid(row=0, column=0)
+urlTb.grid(row=0, column=1)
+findBtn.grid(row=1, columnspan=2)
+
+ws.mainloop()
